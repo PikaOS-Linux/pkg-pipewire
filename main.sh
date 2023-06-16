e174f052fac380996173eeba4f6a2ea5c346f379
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:52:16 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://gitlab.freedesktop.org/pipewire/pipewire.git -b 0.3.71
 cp -rvf ./debian ./pipewire/
@@ -13,8 +8,6 @@ for i in ../patches/*.patch; do patch -Np1 -i $i ;done
 LOGNAME=root dh_make --createorig -y -l -p pipewire_0.3.71
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
